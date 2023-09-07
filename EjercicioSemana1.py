9840b86ce0f26ed0feb369b046ea094dc9a65d4a
Diego Gonzalez <degonzalez6@gmail.com>
2023-09-07 15:55:24 -0300

    Creación de DF 80 y 20 para trabajar

--- EjercicioSemana1.py
+++ EjercicioSemana1.py
@@ -8,18 +8,8 @@ file_name="./Clientes.csv"
 
 column_name = ["Email","Address","Avatar","Avg._Session_Length","Time_on_App","Time_on_Website","Length_of_Membership","Yearly_Amount_Spent"]
 
-df = pd.read_csv(file_name, sep=",")
-nombres_a_cambiar = {
-    '':'ID',
-    'Email':'Email',
-    'Address':'Address',
-    'Avatar':'Avatar',
-    'Avg. Session Length':'Avg_Session_Length',
-    'Time on App':'Time_On_App',
-    'Time on Website':'Time_On_Website',
-    'Length of Membership':'Length_Of_Membership',
-    'Yearly Amount Spent':'Yearly_Amount_Spent'
-}
+df = pd.read_csv(file_name, sep=",", names=column_name)
+
 
 profile = ProfileReport(df, title="Profiling Report", explorative=True,
                         html={"style":{"full_width": True}}, sort=None, progress_bar=False)
@@ -40,27 +30,36 @@ print(df)
 print('df 2 --------------------------------')
 print(df2)
 
+df_train = df2.sample(frac=0.8, random_state=None)
+df_test = df2.sample(frac=0.2, random_state=None)
+
+print('TRAIN------------------------------')
+print(df_train)
+print(df_train.count)
+print('TEST------------------------------')
+print(df_test)
+print(df_test.count)
 # Datos de ejemplo (variables independientes x y variables dependientes y)
-#x = np.array([df])
-#y = np.array([df])
+x = np.array([df_train])
+y = np.array([df_test])
 
 # Crear un objeto de regresión lineal
-#modelo = LinearRegression()
+modelo = LinearRegression()
 
 # Ajustar el modelo a los datos
-#modelo.fit(x.reshape(-1, 1), y)
+modelo.fit(x.reshape(-1, 1), y)
 
 # Realizar predicciones con el modelo entrenado
-#y_pred = modelo.predict(x.reshape(-1, 1))
+y_pred = modelo.predict(x.reshape(-1, 1))
 
 # Visualizar los resultados
-#plt.scatter(x, y, label='Datos reales')
-#plt.plot(x, y_pred, color='red', label='Regresión lineal')
-#plt.xlabel('Variable independiente (x)')
-#plt.ylabel('Variable dependiente (y)')
-#plt.legend()
-#plt.show()
+plt.scatter(x, y, label='Datos reales')
+plt.plot(x, y_pred, color='red', label='Regresión lineal')
+plt.xlabel('Variable independiente (x)')
+plt.ylabel('Variable dependiente (y)')
+plt.legend()
+plt.show()
 
 # Imprimir los coeficientes de la línea recta (pendiente e intercepto)
-#print("Pendiente (coeficiente 'a'): ", modelo.coef_[0])
-#print("Intercepto (coeficiente 'b'): ", modelo.intercept_)
+print("Pendiente (coeficiente 'a'): ", modelo.coef_[0])
+print("Intercepto (coeficiente 'b'): ", modelo.intercept_)
